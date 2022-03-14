ae0254ef764d087ccedd395780b6eb4c6ec97cc7
sineshashi <shashikantrbl123@gmail.com>
2022-03-14 23:27:32 +0530

    Adding some print statement to tests

--- Project/app/test_api.py
+++ Project/app/test_api.py
@@ -12,6 +12,7 @@ def test_listProfile(): # Test for the get request which returns all the profile
     response = requests.get(url)
     assert response.status_code == 200, "Status Code do not match."
     response_data = json.loads(response.text) 
+    print(response_data)
     for profile in response_data: #Verifying whether the age in every profile is greater or equal to zero or not.
         assert profile['age'] >= 0, f"{profile} has invalid age." #Validating age.
 
@@ -29,6 +30,7 @@ def test_postProfile(): #Test the post request for profile.
     response = requests.post(url, json = request_data)
     assert response.status_code == 200, "Sataus Code does not match"
     response_data = json.loads(response.text) #Converting response data to python.
+    print(response_data)
     id = response_data.pop('id') #removing id from the data so that we can match with the request data.
     fields = request_data.keys() #All fields whose data has been passed
     for field in fields: #Checking whether each field of requested data has been updated or not.
@@ -43,6 +45,7 @@ def test_retrieveProfile(id): #Test get request with id.
     response = requests.get(url)
     assert response.status_code == 200, "Status code does not match."
     response_data = json.loads(response.text)
+    print(response_data)
     assert response_data['age'] >= 0, "Invalid age found."
 
 
@@ -50,16 +53,17 @@ def test_retrieveProfile(id): #Test get request with id.
 def test_updateProfile(id): # Test Put request with id.
     url = main_url() + f"profile/{id}"
     request_data = {
-        "name": "Steve Rogers",
-        "checked": False,
-        "age": 25,
-        "description": "Developer",
-        "type": "Python",
+        "name": "Captain",
+        "checked": True,
+        "age": 30,
+        "description": "Soldier",
+        "type": "Commando",
         "date": "2022-03-11T08:50:10.86Z"
     }
     response = requests.put(url, json=request_data)
     assert response.status_code == 200, "Status code does not match."
     response_data = json.loads(response.text)
+    print(response_data)
     reqeusted_fields = list(request_data.keys())
     for field in reqeusted_fields: #Checking whether each field of requested data has been updated or not.
         if field == "date":
@@ -73,6 +77,7 @@ def test_deleteProfile(id): #Test delete request with id.
     response = requests.delete(url)
     assert response.status_code == 200, "Status code does not match."
     response_data = json.loads(response.text)
+    print(response_data)
     assert response_data == f"Profile with id {id} has been deleted.", "No deleted response."
 
 def test_all():
